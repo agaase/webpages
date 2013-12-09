3e4b639eb4cfaa8baeeba7d482fb0a1d36753f70
aseem <aseem@genwi.com>
2013-12-09 12:24:25 +0530

    adding adhesion_raw test

--- demo/js/isonscreen.js
+++ demo/js/isonscreen.js
@@ -1,19 +1,24 @@
-$.fn.isonscreen = function(cont){
-	debugger;
-	var context,tominus=0,toadd=0;
-	if(cont){
-		context = cont;
-		if(!context.isonscreen()){
-			return false;
-		};
-		tominus = context.offset().top;
-	}else{
-		context = $(window);
-		toadd = context.scrollTop();
-	}
+	$.fn.isonscreen = function(context){
+		debugger;
+		var 
+		//Subtract the offset of the parent container.
+		//Will be 0 in case cont is undefined
+		tominus=0,
+		//Add the scrollTop position incase no cont is undefined.
+		toadd=0;
+		if(context){
+			//Find if the div is itself visible
+			if(!context.isonscreen()){
+				return false;
+			};
+			tominus = context.offset().top;
+		}else{
+			context = $(window);
+			toadd = context.scrollTop();
+		}
 
-	if($(this).offset().top - tominus <= (toadd + context.height())){
-		return true;
-	};
-	return false;
-}
+		if($(this).offset().top - tominus <= (toadd + context.height())){
+			return true;
+		};
+		return false;
+	}
