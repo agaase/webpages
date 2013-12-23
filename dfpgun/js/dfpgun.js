a293fc8a06f7e0add90e081b4dbfe3e4fde1bcc8
aseem <aseem@genwi.com>
2013-12-23 13:09:43 +0530

    dfpgun

--- dfpgun/js/dfpgun.js
+++ dfpgun/js/dfpgun.js
@@ -1,10 +1,14 @@
+(function(){
+
+var adslotmap = {};
+
 var loadDFP = function (){
         var DFPLoaded;
         DFPLoaded = DFPLoaded || $('script[src*="gpt.js"]').length;
-    	if(DFPLoaded){
-			return;
-		}
-		window.googletag = window.googletag || {};
+        if(DFPLoaded){
+            return;
+        }
+        window.googletag = window.googletag || {};
         window.googletag.cmd = window.googletag.cmd || [];
         
         var gads = document.createElement('script');
@@ -13,119 +17,62 @@ var loadDFP = function (){
         
         //check for ad blocker
         gads.onerror = function(){
-        	console.log("There is an ad blocker");
+            console.log("There is an ad blocker");
         };
         
         var useSSL = 'https:' === document.location.protocol;
-       // gads.src = (useSSL ? 'https:' : 'http:') +
-        ///'//www.googletagservices.com/tag/js/gpt.js';
-        gads.src="js/gpt.js";
+        gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
         var node = document.getElementsByTagName('script')[0];
         node.parentNode.insertBefore(gads, node);
         
         //another ad block check
         if(gads.style.display === 'none'){
-        	console.log("There is an ad blocker");
+            console.log("There is an ad blocker");
+        }
+    };
+
+var loadedCallback = function(level,message,service,slot,reference){
+    if(message.getMessageId()=="6"){
+        debugger;
+        console.log(message.getMessageArgs());
+        var callback = adslotmap[message.getMessageArgs()]["callback"];
+        if(callback){
+            callback(adslotmap[message.getMessageArgs()]["slot"]);
         }
     }
+};
 
-var triggerDfpAd = function(args){
-            var element = args.element,
+
+$.fn.dfpgun = function(args){
+            loadDFP();
+            var element = $(this),
             adunit=args.adunit,
-            sizes=args.sizes,
-            dontRefresh=args.dontRefresh;
-            googletag=args.googletag || googletag,
-            delayRefresh = args.delayRefresh || 4000; 
+            sizes=args.sizes;
+            googletag=args.googletag || googletag;
 
-            
+            adslotmap[adunit] = {};
+            adslotmap[adunit]["callback"] = args.callback || undefined;
+            adslotmap[adunit]["element"] = element;
+
             console.log("dfploader:Triggering ad-"+adunit);
             var slot;
             var domid = "Ad_"+(parseInt(Math.random()*100000));
             element.empty().attr("id",domid);
-            googletag.debug_log.log = function(level,message,service,slot,reference){
-            };
+            
             googletag.cmd.push(function () {
-        		googletag.pubads().collapseEmptyDivs(true);
-    			googletag.enableServices();
+                googletag.debug_log.log = loadedCallback;
+                googletag.pubads().collapseEmptyDivs(true);
+                googletag.enableServices();
             });
             googletag.cmd.push(function () {
                 slot = googletag.defineSlot(adunit, sizes, domid).addService(googletag.pubads());
+                adslotmap[adunit]["slot"] = slot;
+            });
+            googletag.cmd.push(function () {
+                googletag.display(domid);
             });
             
-            googletag.cmd.push(function () {
-    			googletag.display(domid);
-                if(!dontRefresh){
-                    var count=0;
-                    var timerId = setInterval(function(){
-                        if($("#"+domid).find("iframe").length){
-                            if(!checkIfAdLoaded(domid)){
-                                console.log("dfploader:calling refresh since no ad served; adunit - "+adunit);
-                                googletag.cmd.push(function () {
-                                    googletag.pubads().enableAsyncRendering();
-                                    var slott = googletag.defineSlot(adunit, sizes, domid).addService(googletag.pubads());
-                	               googletag.pubads().refresh([slott]);
-                                   googletag.enableServices();
-                                });
-                            }else{
-                                console.log("dfploader:adunit served successfully for adunit - "+adunit);
-                            }
-                            clearInterval(timerId);
-                        }else if(count>5){
-                            clearInterval(timerId);
-                        }
-                    },delayRefresh);
-                }
-    		});
-            
         };
-        
-        
-var celtraAdhesionLoadedCheck = function(el){
-     if($(".celtra-ad-v3",el).length){
-            return true;
-        }else{
-            return false;
-        }
-};
-var checkIfAdLoaded = function(domid){
-    var adel = $("#"+domid);
-    if(adel.hasClass("floaterAd")){
-       return(celtraAdhesionLoadedCheck(adel));
-    }else{
-        if(adel.is(":visible")){
-            return true;
-        }
-    }
-    return false;
-};
-
-/**
- * pass all the adunits here
- */ 
-var triggerAds = function (adunits) {
-    var timer,i=0,celtraCheckCount=0,adChecked=false;
-    var checkInterstitialTimer = setInterval(function(){
-        celtraCheckCount++;
-        var intEl = $("body .interstitial");
-        if(!intEl.length || !intEl.is(":visible")){
-            clearInterval(checkInterstitialTimer);
-            timer = setInterval(function(){
-                    var ad = adunits[i];
-                    triggerDfpAd({
-                        "element":ad[0],
-                        "adunit":ad[1].id,
-                        "sizes":ad[1].size
-                    });
-                    i++;
-                    if(i>=adunits.length){
-                        clearInterval(timer);
-                    }
-                },300);
-        }else if(!adChecked && !intEl.hasClass("vdopiaad") && celtraCheckCount>7){
-            intEl.hide();
-            adChecked=true;
-        }
-    },500);
-};
-        
-loadDFP();
+})($);
+    
