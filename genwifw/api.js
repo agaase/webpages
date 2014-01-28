835e06a0fb92715f38c9f4ccd837350c35edff63
aseem <aseem@genwi.com>
2014-01-28 16:53:33 +0530

    adding framework docs

---
+++ genwifw/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Genwi.Helpers",
+        "Genwi.Plugins.Imgr",
+        "Genwi.Plugins.Scroller",
+        "Genwi.Plugins.Socialshare",
+        "Genwi.Plugins.Superscroll",
+        "Genwi.Template",
+        "Genwi.Template.AbstractView",
+        "Genwi.Template.Article",
+        "Genwi.Template.Articles",
+        "Genwi.Template.Categories",
+        "Genwi.Template.Favorites",
+        "Genwi.Template.Search",
+        "Genwi.Template.Sections",
+        "Genwi.Template.Toolbar",
+        "Genwi.Utils"
+    ],
+    "modules": [
+        "Genwi"
+    ],
+    "allModules": [
+        {
+            "displayName": "Genwi",
+            "name": "Genwi",
+            "description": "Welcome to the root."
+        }
+    ]
+} };
+});
