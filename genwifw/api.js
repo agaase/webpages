bf3021f63413d2009f794ca8e5be900e936eee9a
aseem <aseem@genwi.com>
2014-01-30 09:42:40 +0530

    removing plugins folder

--- genwifw/api.js
+++ genwifw/api.js
@@ -2,10 +2,6 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Genwi.Helpers",
-        "Genwi.Plugins.Imgr",
-        "Genwi.Plugins.Scroller",
-        "Genwi.Plugins.Socialshare",
-        "Genwi.Plugins.Superscroll",
         "Genwi.Template",
         "Genwi.Template.AbstractView",
         "Genwi.Template.Article",
